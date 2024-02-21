3ddeb3074b0db2c17d3e24e487433504a19ec975
Jordan VanVeldhuisen <jordanvanveldhuisen@gmail.com>
2024-02-21 12:21:15 -0500

    added new properties to classes and added __str__ so I can print out info about animals

---
+++ slithering/__init__.py
@@ -0,0 +1,5 @@
+from .anaconda import Anaconda
+from .copperhead import Copperhead
+from .milksnake import MilkSnake
+from .python import Python
+from .ratsnake import RatSnake
---
+++ walking/__init__.py
@@ -0,0 +1,4 @@
+from .donkey import Donkey
+from .hippo import Hippo
+from .horse import Horse
+from .llama import Llama
